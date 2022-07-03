995af5f857926b1048e06059fcd733b89e1b9b6d
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-03 21:49:56 +0300

    create real copy of object

--- lesson-1/main.js
+++ lesson-1/main.js
@@ -1,11 +1,23 @@
-const user={     // 4567
-    name:'Bob',
+const user = {     // 4567
+    name: 'Bob',
     age: 26,
     isMarried: true
 }
 
-const copyUser  = user
+const copyUser = user  // #4567
 // {} - нет
 // new Object() - нет
 // [] - нет
 // new Array(...) - нет
+
+user.name = 'Anne'
+console.log(copyUser.name)
+
+const realCopyUser = {} // #5666
+realCopyUser.name = user.name
+realCopyUser.age = user.age
+realCopyUser.isMarried = user.isMarried
+
+console.log(realCopyUser)
+console.log(realCopyUser === user) //false
+console.log(copyUser===user) //true
