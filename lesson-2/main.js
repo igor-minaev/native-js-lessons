a223e3bac2b470327b99b8322363f05298b8371c
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-05 09:54:20 +0300

    lesson 2

---
+++ lesson-2/main.js
@@ -0,0 +1,39 @@
+const students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    }
+];
+
+// const names=['Bob','Alex','Nick','John']
+
+const getNames = (arr) => {
+    const result = []
+    const func = st => st.name
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])
+        result.push(newValue)
+    }
+    return result
+}
+console.log(getNames(students))
