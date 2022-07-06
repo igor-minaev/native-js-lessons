ca486b4c8205ff4b700b421bd20efe7c979d90d4
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-06 21:39:42 +0300

    add stopPropogation

--- lesson-3/main.js
+++ lesson-3/main.js
@@ -4,8 +4,23 @@
 // {} (сведения о произошедшем событии) -> event, ev, e, evt
 
 const sm = document.getElementById('small')
+const md = document.getElementById('medium')
+const bg = document.getElementById('big')
 
-function onCllickHandler(){
-    alert('hey!')
+function onCllickHandlerSm(e) {
+    e.stopPropagation()  //прекратить распространение
+    alert('yo')
 }
-sm.onclick= onCllickHandler
+
+// sm.onclick = onCllickHandler  // старый метод
+// sm.onclick = null   // удаляем обработчик
+
+sm.addEventListener('click', onCllickHandlerSm)  // новый метод
+// sm.removeEventListener('click', onCllickHandler) // удаляем обработчик
+
+md.addEventListener('click', (e) => {
+        e.stopPropagation()
+        console.log(e.currentTarget.id)
+    }
+)
+bg.addEventListener('click', (e) => console.log('yo'))
