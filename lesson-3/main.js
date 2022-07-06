02108cd8fac2b997e2fa3ad126baa6d3c02e1ee7
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-06 20:24:50 +0300

    add handler

--- lesson-3/main.js
+++ lesson-3/main.js
@@ -1,1 +1,11 @@
-// обработчик, handler, подписчик, subscriber, listener, слушатель
+// обработчик, handler, подписчик, subscriber, listener, слушатель --> function
+// Handler будет вызванан при наступлении события
+// Handler будет вызванан с аргументом ({})
+// {} (сведения о произошедшем событии) -> event, ev, e, evt
+
+const sm = document.getElementById('small')
+
+function onCllickHandler(){
+    alert('hey!')
+}
+sm.onclick= onCllickHandler
