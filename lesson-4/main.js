c6c3956766c4cd3c3e5ef468510b5da3cbbde46d
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-12 11:26:53 +0300

    add id

--- lesson-4/main.js
+++ lesson-4/main.js
@@ -1,11 +1,14 @@
+const todoListID_1 = 'qwer'
+const todoListID_2 = 'asdf'
+
 const todoLists = [
     {
-        id: 1,
+        id: todoListID_1,
         title: 'What to learn',
         filter: 'all',
     },
     {
-        id: 1,
+        id: todoListID_2,
         title: 'What to buy',
         filter: 'all',
     }
@@ -13,12 +16,12 @@ const todoLists = [
 
 
 const tasks = {
-    [todoLists[0].id]: [
+    [todoListID_1]: [
         {id: 12, title: 'JS', isDone: false},
         {id: 13, title: 'ТS', isDone: false},
         {id: 14, title: 'HTML', isDone: false}
     ],
-    [todoLists[2].id]: [
+    [todoListID_2]: [
         {id: 22, title: 'Milk', isDone: false},
         {id: 23, title: 'Cheese', isDone: false},
         {id: 24, title: 'Ice-cream', isDone: false}
