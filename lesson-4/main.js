241c8c0bc13d378ca863e6d832afc8bca666f573
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-13 12:42:36 +0300

    refactore code

--- lesson-4/main.js
+++ lesson-4/main.js
@@ -43,24 +43,28 @@ console.log(nums.reduce((acc, el) => acc > el ? acc : el))
 
 const students = [
     {
+        id: 1,
         name: "Bob",
         age: 22,
         isMarried: true,
         scores: 85,
     },
     {
+        id: 2,
         name: "Alex",
         age: 21,
         isMarried: true,
         scores: 89
     },
     {
+        id: 3,
         name: "Nick",
         age: 20,
         isMarried: false,
         scores: 120
     },
     {
+        id: 4,
         name: "John",
         age: 19,
         isMarried: false,
@@ -75,3 +79,18 @@ console.log(students.reduce((acc, el) => {
     acc.push({...el, scores: el.scores + 10})
     return acc
 }, []))
+
+const st = {
+    1: {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    }
+}
+
+console.log(students.reduce((acc, el) => {
+    acc[el.id] = {...el}
+    delete acc[el.id].id
+    return acc
+}, {}))
