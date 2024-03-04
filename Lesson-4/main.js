fee625749b865bed15c75360b9444d1a6720017e
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-04 20:05:08 +0300

    start lesson 4

---
+++ Lesson-4/main.js
@@ -0,0 +1,31 @@
+const todoLists = [
+    {
+        id: 1,
+        title: 'What to learn',
+        filter: 'all',
+        // tasks: [
+        //     {id: 3, title: 'HTML', isDone: false},
+        //     {id: 4, title: 'CSS', isDone: false},
+        //     {id: 5, title: 'REACT', isDone: false}
+        // ]
+    },
+    {
+        id: 1,
+        title: 'What to buy',
+        filter: 'all',
+        // tasks: [
+        //     {id: 6, title: 'MILK', isDone: false},
+        //     {id: 7, title: 'BEER', isDone: false},
+        //     {id: 8, title: 'MEAT', isDone: false}
+        // ]
+    }
+]
+
+const tasks = [
+    {todoListId: 1, id: 3, title: 'HTML', isDone: false},
+    {todoListId: 1, id: 4, title: 'CSS', isDone: false},
+    {todoListId: 1, id: 5, title: 'REACT', isDone: false},
+    {todoListId: 2, id: 6, title: 'MILK', isDone: false},
+    {todoListId: 2, id: 7, title: 'BEER', isDone: false},
+    {todoListId: 2, id: 8, title: 'MEAT', isDone: false}
+]
